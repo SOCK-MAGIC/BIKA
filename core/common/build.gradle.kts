4944dc5683c1201118346df84fe76184a27b76a6
sock_magic <startxingcheng@gmail.com>
2024-09-27 11:08:32 +0800

    refactor: 优化

--- core/common/build.gradle.kts
+++ core/common/build.gradle.kts
@@ -1,9 +1,11 @@
 plugins {
-    alias(libs.plugins.bika.jvm.library)
+    alias(libs.plugins.bika.android.library)
     alias(libs.plugins.bika.hilt)
 }
 
 dependencies {
+    implementation("app.cash.molecule:molecule-runtime:2.0.0")
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.decompose)
     implementation(libs.kotlinx.coroutines.core)
     implementation(libs.kotlinx.serialization.json)
