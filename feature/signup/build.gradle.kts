e41ea985320b896185e3d558ce880ef3c61afab5
sock_magic <startxingcheng@gmail.com>
2024-09-27 10:53:35 +0800

    refactor: 注册

---
+++ feature/signup/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    alias(libs.plugins.bika.android.feature)
+    alias(libs.plugins.bika.android.compose)
+    alias(libs.plugins.bika.hilt)
+}
+
+android {
+    namespace = "com.shizq.bika.feature.signup"
+}
+
+dependencies {
+    implementation(projects.core.datastore)
+    implementation(projects.core.network)
+
+    implementation(libs.decompose)
+}
---
+++ feature/signup/src/main/kotlin/com/shizq/bika/feature/signup/SignUpComponentImpl.kt
@@ -0,0 +1,42 @@
+package com.shizq.bika.feature.signup
+
+import com.arkivanov.decompose.ComponentContext
+import com.shizq.bika.core.component.componentScope
+import com.shizq.bika.core.datastore.BikaPreferencesDataSource
+import com.shizq.bika.core.datastore.model.Account
+import com.shizq.bika.core.network.BikaNetworkDataSource
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+class SignUpComponentImpl @AssistedInject constructor(
+    @Assisted componentContext: ComponentContext,
+    private val network: BikaNetworkDataSource,
+    private val preferencesDataSource: BikaPreferencesDataSource
+) : SignUpComponent, ComponentContext by componentContext {
+    val acc = preferencesDataSource.userData.map { preferences ->
+        preferences.account?.let { JVMD.Success(it) } ?: JVMD.Failed
+    }.stateIn(
+        componentScope,
+        SharingStarted.WhileSubscribed(5000),
+        JVMD.Failed
+    )
+    fun signIn(email: String, password: String) {
+        // network.signIn(email,password)
+    }
+
+    @AssistedFactory
+    interface Factory : SignUpComponent.Factory {
+        override fun invoke(
+            componentContext: ComponentContext,
+        ): SignUpComponentImpl
+    }
+}
+
+sealed interface JVMD {
+    data class Success(val account: Account) : JVMD
+    data object Failed : JVMD
+}
