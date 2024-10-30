d2842caf0b2856f00693b6c846c01c0f6cc6c453
sock_magic <startxingcheng@gmail.com>
2024-10-30 10:50:41 +0800

    refactor: 优化

--- feature/comment/src/main/kotlin/com/shizq/bika/feature/comment/CommentComponentImpl.kt
+++ feature/comment/src/main/kotlin/com/shizq/bika/feature/comment/CommentComponentImpl.kt
@@ -1,8 +1,6 @@
 package com.shizq.bika.feature.comment
 
 import com.arkivanov.decompose.ComponentContext
-import com.shizq.bika.core.datastore.BikaPreferencesDataSource
-import com.shizq.bika.core.network.BikaNetworkDataSource
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
--- feature/comment/src/main/kotlin/com/shizq/bika/feature/comment/CommentScreen.kt
+++ feature/comment/src/main/kotlin/com/shizq/bika/feature/comment/CommentScreen.kt
@@ -6,10 +6,8 @@ import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconToggleButton
 import androidx.compose.material3.ListItem
