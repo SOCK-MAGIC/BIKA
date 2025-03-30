bca95f009624254befd6c6ed3d876bd225b5cc8c
sock_magic <startxingcheng@gmail.com>
2025-03-31 00:09:14 +0800

    refactor(feature/signin): 重命名 SignInModule 为 LoginModule- 将 SignInModule 接口重命名为 LoginModule - 更新文件名以匹配新的接口名称 - 此更改是为了更好地反映模块的功能，提高代码的可读性和一致性

--- feature/signin/src/main/kotlin/com/shizq/bika/feature/signin/di/LoginModule.kt
+++ feature/signin/src/main/kotlin/com/shizq/bika/feature/signin/di/LoginModule.kt
@@ -9,7 +9,7 @@ import dagger.hilt.components.SingletonComponent
 
 @Module
 @InstallIn(SingletonComponent::class)
-interface SignInModule {
+interface LoginModule {
     @Binds
     fun componentFactory(impl: SignInComponentImpl.Factory): SignInComponent.Factory
 }
